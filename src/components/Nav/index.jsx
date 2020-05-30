b7dd09941301a662c8d1432916dbfc8735821e9e
CaisanLi <18081007254@sina.cn>
2020-05-30 23:21:13 +0800

    增加导航组件

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 // css module
 import style from "./index.module.less";
-console.log(style);
 export default function() {
     return (
         <div className={ style.box }></div>
