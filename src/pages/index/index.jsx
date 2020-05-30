fe7442013c15aac4b10bab3e76bbe2c8c2c5574d
CaisanLi <18081007254@sina.cn>
2020-05-30 23:23:03 +0800

    调整首页布局样式

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -5,13 +5,17 @@ import { Switch, Route } from "react-router-dom";
 import Nav from "@comp/Nav";
 // 页面
 import Editor from '@pages/editor';
+// 样式
+import style from './index.module.less';
 export default function Index() {
     return (
-        <div className={'csl-container'}>
-            <Nav />
-            <Switch>
-                <Route path="/" component={ Editor }></Route>
-            </Switch>
+        <div className={style.cslContainer}>
+            <Nav className={style.cslNav} />
+            <section className={style.cslSection}>
+              <Switch>
+                  <Route path="/" component={ Editor }></Route>
+              </Switch>
+            </section>
         </div>
     )
 }
