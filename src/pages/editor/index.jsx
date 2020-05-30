2079b7929f82ce290d44bf5c4d5e107d81fe0ad3
CaisanLi <18081007254@sina.cn>
2020-05-30 23:25:21 +0800

    调整布局

--- src/pages/editor/index.jsx
+++ src/pages/editor/index.jsx
@@ -1,6 +1,9 @@
 // 编辑页面
 import React from 'react';
-
+import style from "./index.module.less";
+// 组件
+import ToolBar from '@comp/ToolBar';
+import Scroll from '@comp/Scroll';
 /**
  * 整体布局：
  * 1、上方工具栏
@@ -10,6 +13,24 @@ import React from 'react';
  */
 export default function() {
     return (
-        <div>Editor</div>
+        <div className={style.editorBox}>
+          {/* 工具栏 */}
+          <div className={style.editorTools}>
+            <ToolBar />
+          </div>
+          {/* 容器 */}
+          <div className={style.editorContent}>
+            {/* 图形选择栏 */}
+            <div className={style.editorLeftAside}>
+                <Scroll visible={false}>
+                    <div style={{height: 7000+'px'}}></div>
+                </Scroll>
+            </div>
+            {/* 编辑区域 */}
+            <div className={style.editorWarp}></div>
+            {/* 浮动工具栏 */}
+            <div className={style.editorRightAside}></div>
+          </div>
+        </div>
     )
 }
