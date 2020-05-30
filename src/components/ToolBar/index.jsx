e1afe44b613921df06fa1d13746f91cddac1bfc5
CaisanLi <18081007254@sina.cn>
2020-05-30 23:30:23 +0800

    增加工具栏组件(10%)

---
+++ src/components/ToolBar/index.jsx
@@ -0,0 +1,25 @@
+// 工具栏
+import React from 'react'
+// 样式
+import style from './index.module.less';
+// 导入功能按钮
+import tools from '@/assets/json/tools.json';
+console.log(tools)
+export default function ({ click }) {
+    return (
+        <div className={style.toolBarBox}>
+            {/* 常用功能 */}
+            <div className={style.tools}>
+                {tools.map(t => {
+                    return (
+                        <div onClick={() => click(t)} className={[style.tool, t.interval ? style.interval : ''].join(' ')}>
+                            {t.name}
+                        </div>
+                    )
+                })}
+            </div>
+            {/* 辅助功能 */}
+            <div></div>
+        </div>
+    );
+}
