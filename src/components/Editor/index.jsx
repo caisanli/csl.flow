efe21febaad297facc3e29240690c97f59c8a9c7
caigx <caigx@zxjmail.com>
2020-06-04 17:58:33 +0800

    添加编辑框组件

---
+++ src/components/Editor/index.jsx
@@ -0,0 +1,24 @@
+// 编辑框
+import React from 'react';
+// 组件
+import Scroll from '@comp/Scroll';
+// 样式
+import style from './index.module.less';
+export default class Editor extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        let width = 1000 + 'px';
+        let height = 800 + 'px'
+        return (
+            <div className={style.editorBox} >
+                <Scroll>
+                    <div className={style.editorContainer}>
+                        <div style={{width, height}}></div>
+                    </div>
+                </Scroll>
+            </div>
+        );
+    }
+}
