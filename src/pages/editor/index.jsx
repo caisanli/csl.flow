6f5a6d129d2d265d7fc5bd841adbea4c3582f860
caigx <caigx@zxjmail.com>
2020-06-04 18:00:28 +0800

    增加编辑框区域

--- src/pages/editor/index.jsx
+++ src/pages/editor/index.jsx
@@ -1,10 +1,11 @@
 // 编辑页面
-import React from 'react';
+import React, {useRef} from 'react';
 import style from "./index.module.less";
 // 组件
 import ToolBar from '@comp/ToolBar';
 import Scroll from '@comp/Scroll';
 import { Collapse, Panel } from "@comp/Collapse";
+import Editor from "@comp/Editor";
 // 图形组件
 import Bases from '@comp/Graph/base';
 
@@ -16,45 +17,111 @@ import Bases from '@comp/Graph/base';
  * 3、中间操作区域
  * 4、右侧小工具栏
  */
-export default function() {
-    return (
-        <div className={style.editorBox}>
-          {/* 工具栏 */}
-          <div className={style.editorTools}>
-            <ToolBar />
-          </div>
-          {/* 容器 */}
-          <div className={style.editorContent}>
-            {/* 图形选择栏 */}
-            <div className={style.editorLeftAside}>
-                <Scroll visible={false}>
-                    <Collapse defaultActiveKeys={['1']}>
-                        <Panel header={'基础图形'} key={'1'}>
-                            {
-                                Bases.map((b, i) => <Graph key={i} Comp={b.Comp} />)
-                            }
-                        </Panel>
-                        <Panel header={'FlowChart流程图'} key={'2'}>
-                            <p>这是内容01</p>
-                        </Panel>
-                        <Panel header={'EVC企业价值链'} key={'3'}>
-                            <p>这是内容01</p>
-                        </Panel>
-                    </Collapse>
-                </Scroll>
+export default class EditorBox extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            detailY: 0,
+            isDetail: false,
+            detailGraph: null
+        }
+        // ref
+        this.asideRef = React.createRef();
+        // 绑定方法
+        this.onMouseEnter = this.onMouseEnter.bind(this);
+        this.onMouseLeave = this.onMouseLeave.bind(this);
+    }
+    onMouseEnter(e, graph) {
+        console.log('进来了');
+        this.setState({
+            detailY: e.pageY,
+            detailVisible: true,
+            detailGraph: graph
+        });
+    }
+    onMouseLeave() {
+        this.setState({
+            detailVisible: false
+        })
+    }
+    render() {
+        return (
+            <div className={style.editorBox}>
+              {/* 工具栏 */}
+              <div className={style.editorTools}>
+                <ToolBar />
+              </div>
+              {/* 容器 */}
+              <div className={style.editorContent}>
+                {/* 图形选择栏 */}
+                <div className={style.editorLeftAside} ref={this.asideRef}>
+                    <GraphDetail y={this.state.detailY} graph={this.state.detailGraph} visible={this.state.detailVisible}/>
+                    <Scroll>
+                        <Collapse defaultActiveKeys={['1']}>
+                            <Panel header={'基础图形'} key={'1'}>
+                                {
+                                    Bases.map(
+                                        (b, i) => <Graph enter={(e) => this.onMouseEnter(e, b)} 
+                                                            leave={this.onMouseLeave} 
+                                                            key={i} 
+                                                            comp={b.comp} />
+                                    )
+                                }
+                            </Panel>
+                            <Panel header={'FlowChart流程图'} key={'2'}>
+                                <p>这是内容01</p>
+                            </Panel>
+                            <Panel header={'EVC企业价值链'} key={'3'}>
+                                <p>这是内容01</p>
+                            </Panel>
+                        </Collapse>
+                    </Scroll>
+                </div>
+                {/* 编辑区域 */}
+                <div className={style.editorWarp}>
+                    <Editor />
+                </div>
+                {/* 浮动工具栏 */}
+                <div className={style.editorRightAside}></div>
+              </div>
             </div>
-            {/* 编辑区域 */}
-            <div className={style.editorWarp}></div>
-            {/* 浮动工具栏 */}
-            <div className={style.editorRightAside}></div>
-          </div>
+        )
+    }
+}
+// 图形缩略图
+function Graph(props) {
+    let { enter, leave, comp } = props;
+    let Comp = comp;
+    return (
+        <div className={style.graph} onMouseEnter={enter} onMouseLeave={leave}>
+            <Comp width={40} height={40} strokeWidth={30} />
         </div>
     )
 }
-function Graph(props) {
+
+// 图形详情
+function GraphDetail(props) {
+    let { y, visible, graph } = props;
+    if(!visible) return null;
+    let Comp = graph.comp;
+    let title = graph.title;
+    let top = getDetailTop(y);
     return (
-        <div className={style.graph}>
-            <props.Comp width={40} height={40} strokeWidth={30} />
+        <div className={style.graphDetailBox} style={{top: top + 'px'}}>
+            <div className={style.graphDetailContent}>
+                <div className={style.graphDetailSvg}>
+                    <Comp width={120} height={90} strokeWidth={15} stroke={'#244462'}/>
+                </div>
+                <p className={style.graphDetailTitle}>{title}</p>
+            </div>
         </div>
     )
+}
+// 获取图形详情top值
+function getDetailTop(y) {
+    let bh = document.body.offsetHeight;
+    let eh = 142;
+    let top = y - eh / 2;
+    top = top + eh / 2 > bh ? bh - eh : top < 0 ? 0 : top;
+    return top;
 }
