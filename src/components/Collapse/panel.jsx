0f874e378c3f90166fafb239997118f4f81512f7
caigx <caigx@zxjmail.com>
2020-06-04 17:58:17 +0800

    删除输入日志

--- src/components/Collapse/panel.jsx
+++ src/components/Collapse/panel.jsx
@@ -3,7 +3,6 @@ import PropTypes from "prop-types";
 // 样式
 import style from './panel.module.less'
 function Panel(props) {
-    console.log(this)
     let {header, children, onItemClick, isActive} = props;
     return (
         <div className={[style.item, isActive ? style.active : ''].join(' ')}>
