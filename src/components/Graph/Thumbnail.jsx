5222cc742f6a39b1416c715670037999e70a3000
caigx <caigx@zxjmail.com>
2020-06-08 16:43:46 +0800

    添加图形列表组件

---
+++ src/components/Graph/Thumbnail.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// 样式
+import style from './Thumbnail.module.less';
+export default function(props) {
+    return (
+        <div draggable={true} >
+
+        </div>
+    );
+}
