56f2fdf3208c3b8fced6625c26b1070e904bec8c
CaisanLi <18081007254@sina.cn>
2020-06-09 22:38:16 +0800

    添加drap事件

--- src/components/Graph/Thumbnail.jsx
+++ src/components/Graph/Thumbnail.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 // 样式
 import style from './Thumbnail.module.less';
-export default function(props) {
-    return (
-        <div draggable={true} >
 
+export default function(props) {
+    let {enter, leave, dragStart, drag, dragEnd} = props;
+    return (
+        <div >
+            {
+                props.graphs.map((g,i) => {
+                    let Comp = g.comp;
+                    return (
+                    <div key={i}
+                        className={style.graph} 
+                        draggable={true}
+                        onDrag={e => drag && drag(g, e)}
+                        onDragStart={e => dragStart && dragStart(g, e)}
+                        onDragEnd={dragEnd}
+                        onMouseEnter={e => enter && enter(g, e)} 
+                        onMouseLeave={e => leave && leave(g, e)} >
+                        <Comp width={40} height={40} strokeWidth={30} />
+                    </div>
+                    )
+                })
+            }
         </div>
     );
 }
