9b5f2e58e815a388e357a03739ec40a52df1da3c
CaisanLi <18081007254@sina.cn>
2020-06-21 23:45:51 +0800

    添加浅拷贝和深拷贝

--- src/utils/index.js
+++ src/utils/index.js
@@ -14,3 +14,28 @@ export function getOffset(elem) {
     }
     return { top, left }
 }
+
+export function clone(obj) {
+    if(typeof obj !== 'object') 
+        return obj;
+    let newObj = {};
+    for (const key in obj) {
+        if (object.hasOwnProperty(key)) {
+            newObj[key] = object[key];
+        }
+    }
+    return newObj;
+}
+// 深拷贝
+export function deepClone(obj) {
+    if(typeof obj !== 'object') 
+        return obj;
+    const isArray = Array.isArray(obj);
+    let newObj = isArray ? [] : {};
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            newObj[key] = deepClone(obj[key]);
+        }
+    }
+    return newObj;
+}
