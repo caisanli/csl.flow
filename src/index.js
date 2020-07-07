ff95d42706d9102dd94ce4e586bc1114b4bb1e64
CaisanLi <18081007254@sina.cn>
2020-07-07 23:22:21 +0800

    添加react redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import { BrowserRouter as Router } from "react-router-dom";
+// reducers
+import reducers from './reducers'
 // 通用样式
 import '@/assets/css/theme.less';
 import '@/assets/css/reset.less';
 import '@/assets/css/base.less';
 // 组件
 import Index from './pages/index';
-
+let store = createStore(reducers)
 ReactDom.render(
-    <Router>
-        <Index />
-    </Router>, 
+    <Provider store={store}>
+        <Router>
+            <Index />
+        </Router>
+    </Provider>
+    ,
     document.getElementById("root")
 );
 if (module.hot) {  
