3c7d712294d58069695eca9e390e974dfae5c44b
CaisanLi <18081007254@sina.cn>
2020-07-08 22:59:17 +0800

    添加按钮组件

---
+++ src/components/ToolBar/button.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import style from './button.module.less';
+export default function Button(props) {
+    return (
+        <div className={[style.button, props.disabled ? 'disable' : ''].join(' ')}>
+            { props.icon && <span className={['iconfont', props.icon].join(' ')}></span> }
+            { props.children }
+        </div>
+    )
+}
+Button.propTypes = {
+    disabled: PropTypes.bool,
+    icon: PropTypes.string
+}
