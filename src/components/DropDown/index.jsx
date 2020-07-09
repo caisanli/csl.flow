9fbd06a6393cc3cc01c9656bf08f51abf984d98c
CaisanLi <18081007254@sina.cn>
2020-07-09 22:42:32 +0800

    调整dropdown组件

--- src/components/DropDown/index.jsx
+++ src/components/DropDown/index.jsx
@@ -1,26 +1,49 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import PropTypes from 'prop-types';
 import style from './index.module.less';
+import { getParent } from "@/utils";
 export default function DropDown(props) {
-
+    const [visible, setVisible] = useState(false);
+    const [top, setTop] = useState(0);
+    const boxRef = useRef(null);
+    function onClick() {
+        if(props.disabled)
+            return ;
+        setVisible(!visible)
+    }
+    function close() {
+        setVisible(false)
+    }
+    function clickBody(e) {
+        let className = e.target.className;
+        if(className && (className.includes(style.dropdownWarp) || getParent(e.target, `.${style.dropdownWarp}`))) 
+            return ;
+        setVisible(false);
+    }
+    useEffect(() => {
+        let top = boxRef.current.offsetHeight + 3;
+        setTop(top);
+        props.onRef && props.onRef({close});
+        document.body.addEventListener('click', clickBody);
+        return function () {
+            document.body.removeEventListener('click', clickBody);
+        }
+    })
     return (
-        <div className={style.dropdown}>
-            <div className={style.dropdownLabel}>
+        <div className={style.dropdown} ref={boxRef}>
+            <div className={[style.dropdownLabel, visible ? style.hover : ''].join(' ')} onClick={onClick}>
                 <div className={style.dropdownLabelText}>
                     { props.text }
                 </div>
                 <div className={style.dropdownLabelIcon}></div>
             </div>
             {
-                props.visible && <div className={style.dropdownContent}>
-                                    <div className={style.dropdownWarp}>
-                                        { props.content }
-                                    </div>
+                visible && <div className={style.dropdownContent} style={{top: top + 'px'}}>
+                                <div className={style.dropdownWarp}>
+                                    { props.content }
                                 </div>
+                            </div>
             }
         </div>
     )
-}
-DropDown.propTypes = {
-    visible: PropTypes.bool.isRequired
 }
