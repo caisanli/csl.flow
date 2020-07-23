df514f09eb9d3b1cfa34ab101e8d1f2165d67a00
CaisanLi <18081007254@sina.cn>
2020-07-23 22:39:41 +0800

    增加可选参数

--- src/components/Graph/Svg.jsx
+++ src/components/Graph/Svg.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
 // svg基础组件
 function Svg (props) {
-    let { width, height, viewBox, fill, stroke, strokeWidth } = props;
+    // let { width, height, viewBox, fill, stroke, strokeWidth, strokeDasharray } = props;
     return (
         <svg xmlns="http://www.w3.org/2000/svg" 
-            width={width} 
-            height={height} 
-            viewBox={viewBox} 
-            fill={fill} 
-            stroke={stroke} 
-            strokeWidth={strokeWidth}
+            {...props}
             version="1.1"
         >
             {props.children}
@@ -20,6 +15,7 @@ Svg.defaultProps = {
     width: 120,
     height: 120,
     fill: 'none',
+    strokeDasharray: '',
     stroke: '#000',
     strokeWidth: '5',
     viewBox: '0 0 1024 1024'
